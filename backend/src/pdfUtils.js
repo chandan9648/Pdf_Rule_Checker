402e1e7fb9c77ea3629e0fc6d16345581597ed26
Chandan Chaudhary <chandankkumar156@gmail.com>
2025-11-22 01:09:05 +0530

    update dependencies and refactor PDF handling to use pdf-parse

--- backend/src/pdfUtils.js
+++ backend/src/pdfUtils.js
@@ -1,7 +1,7 @@
 // pdfUtils.js
 import fs from "fs";
 import path from "path";
-import { getDocument } from "pdfjs-dist";
+import { getDocument } from "pdf-parse";
 import { TextDecoder } from "util";
 
 // pdfjs-dist expects a Node-compatible fetch; getDocument accepts file path in Node
